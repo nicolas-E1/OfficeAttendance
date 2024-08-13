598a90413ff0a0e6e78a4cd1ff6a0e61d2d41552
Nicolas Bracigliano <me@nicolasbracigliano.com>
2024-08-13 14:50:29 +1000

    Update FakeAttendanceRepository to support setting attendance for a specific week
    This commit updates the FakeAttendanceRepository in the OfficeAttendanceAPI.Tests.Application.Fakes namespace. It adds a new method, SetAttendanceForWeek, which allows setting the attendance for a specific week. This enhancement improves the flexibility of the FakeAttendanceRepository for testing purposes, enabling the simulation of different attendance scenarios for specific weeks.

--- OfficeAttendanceAPI.Tests/Application/Fakes/FakeAttendanceRepository.cs
+++ OfficeAttendanceAPI.Tests/Application/Fakes/FakeAttendanceRepository.cs
@@ -1,24 +1,32 @@
 using OfficeAttendanceAPI.Core.Entities;
+using OfficeAttendanceAPI.Core.Exceptions.Attendance;
 using OfficeAttendanceAPI.Core.Interfaces;
 
 namespace OfficeAttendanceAPI.Tests.Fakes
 {
     public class FakeAttendanceRepository : IAttendanceRepository
     {
-        private List<Employee> _attendance = new();
+        private readonly Dictionary<int, List<Employee>> _attendance = [];
         public bool WasGetByWeekCalled { get; private set; } = false;
-        
-        public void SetAttendance(IEnumerable<Employee> attendance)
-        {
-            _attendance = new List<Employee>(attendance);
-        }
+        public bool ShouldThrowException { get; set; } = false;
+        private int _currentWeek = 1;
+
+        public void SetAttendance(IEnumerable<Employee>? attendance) => _attendance[_currentWeek] =  attendance?.ToList() ?? [];
+
+        public void SetAttendanceForWeek(IEnumerable<Employee> attendance, int week) => _attendance[week] =  attendance?.ToList() ?? [];
+
+        public void SetCurrentWeek(int week) => _currentWeek = week;
 
         public Task<IEnumerable<Employee>> GetByDay(DateOnly date, CancellationToken ct) => throw new NotImplementedException();
 
         public Task<IEnumerable<Employee>> GetByWeek(CancellationToken cancellationToken)
         {
             WasGetByWeekCalled = true;
-            return Task.FromResult<IEnumerable<Employee>>(_attendance);
+            if (ShouldThrowException) {
+                throw new AttendanceNotFoundException("Failed to get attendance by week");
+            }
+            _attendance.TryGetValue(_currentWeek, out List<Employee>? attendance);
+            return Task.FromResult<IEnumerable<Employee>>(attendance ?? []);
         }
 
         public Task<IEnumerable<Attendance>> GetByUserId(int id, CancellationToken ct) => throw new NotImplementedException();
