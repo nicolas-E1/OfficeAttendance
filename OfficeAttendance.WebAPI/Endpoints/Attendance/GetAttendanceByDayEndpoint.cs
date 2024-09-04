0fd7de04372575b7f5b15d918765b44a75b32822
Nicolas Bracigliano <me@nicolasbracigliano.com>
2024-09-04 18:43:44 +1000

    refactor: Rename GetAttendanceByDayEndpoint and GetAttendanceByWeekEndpoint to use common namespace

--- OfficeAttendance.WebAPI/Endpoints/Attendance/GetAttendanceByDayEndpoint.cs
+++ OfficeAttendance.WebAPI/Endpoints/Attendance/GetAttendanceByDayEndpoint.cs
@@ -2,7 +2,7 @@
 using OfficeAttendance.Application.DTOs.Attendance;
 using OfficeAttendance.Application.UseCases.Attendance;
 
-namespace OfficeAttendance.WebAPI.Endpoints.Employee.Attendance;
+namespace OfficeAttendance.WebAPI.Endpoints.Attendance;
 
 public class GetAttendanceByDayEndpoint(GetAttendanceByDayUseCase getAttendanceByDayUseCase) : Endpoint<GetByDayRequest, GetByDayResponse> {
     public override void Configure() {
@@ -11,7 +11,7 @@ public class GetAttendanceByDayEndpoint(GetAttendanceByDayUseCase getAttendanceB
     }
 
     public override async Task HandleAsync(GetByDayRequest request, CancellationToken ct) {
-        var response = await getAttendanceByDayUseCase.ExecuteAsync(request, ct);
+        GetByDayResponse response = await getAttendanceByDayUseCase.ExecuteAsync(request, ct);
         await SendAsync(response, cancellation: ct);
     }
 }
