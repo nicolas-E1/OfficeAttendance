ce62e72cde6bfe407dccb65580fcfc2c0303bd01
Nicolas Bracigliano <me@nicolasbracigliano.com>
2024-09-04 11:10:17 +1000

    refactor: Rename the options and settings variable to not use single letters.
    What was I thinking?!

--- OfficeAttendance.WebAPI/Program.cs
+++ OfficeAttendance.WebAPI/Program.cs
@@ -1,4 +1,4 @@
-using FastEndpoints;
+﻿using FastEndpoints;
 using FastEndpoints.Swagger;
 using Microsoft.EntityFrameworkCore;
 using OfficeAttendance.Application.UseCases.Attendance;
@@ -25,13 +25,13 @@ builder.Services.AddScoped<GetAttendanceByWeekUseCase>();
 
 builder.Services.AddFastEndpoints();
 builder.Services.AddDbContext<AppDbContext>(options => {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    _ = options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.SwaggerDocument(o => {
-    o.DocumentSettings = s => {
-        s.Title = "Office Attendance API";
-        s.Version = "v1";
-        s.Description = "API to help you manage your office attendance. It allows you to manage employees and their attendance. It also allows you to know who's going to the office each day.";
+builder.Services.SwaggerDocument(options => {
+    options.DocumentSettings = settings => {
+        settings.Title = "Office Attendance API";
+        settings.Version = "v1";
+        settings.Description = "API to help you manage your office attendance. It allows you to manage employees and their attendance. It also allows you to know who's going to the office each day.";
     };
 });
 
