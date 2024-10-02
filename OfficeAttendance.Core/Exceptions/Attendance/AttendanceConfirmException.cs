e0559e0cde7d3ea74d88271afbf98ff5002a3a49
Nicolas Bracigliano <me@nicolasbracigliano.com>
2024-10-02 17:29:01 +1000

    Refactor AttendanceConfirmException class to use SetAttendanceException

--- OfficeAttendance.Core/Exceptions/Attendance/AttendanceConfirmException.cs
+++ OfficeAttendance.Core/Exceptions/Attendance/AttendanceConfirmException.cs
@@ -1,4 +1,8 @@
 ﻿namespace OfficeAttendance.Core.Exceptions.Attendance;
 
-public class AttendanceConfirmException(string message, Exception innerException)
-    : Exception(message, innerException);
+public class SetAttendanceException : Exception {
+    public SetAttendanceException(string message) : base(message) {
+    }
+    public SetAttendanceException(string message, Exception innerException) : base(message, innerException) {
+    }
+};
