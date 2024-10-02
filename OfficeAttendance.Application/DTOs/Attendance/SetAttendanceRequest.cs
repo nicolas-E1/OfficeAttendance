cf9e33ba259c62a0b6ca014dcb17ef51a781c714
Nicolas Bracigliano <me@nicolasbracigliano.com>
2024-10-02 17:39:36 +1000

    Create SetAttendanceRequest and SetAttendanceResponse classes

---
+++ OfficeAttendance.Application/DTOs/Attendance/SetAttendanceRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace OfficeAttendance.Application.DTOs.Attendance;
+
+public class SetAttendanceRequest {
+    public int EmployeeId { get; set; }
+    public DateOnly Date { get; set; }
+}
---
+++ OfficeAttendance.Application/DTOs/Attendance/SetAttendanceResponse.cs
@@ -0,0 +1,8 @@
+﻿using OfficeAttendance.Core.Entities;
+
+namespace OfficeAttendance.Application.DTOs.Attendance;
+
+public class SetAttendanceResponse {
+    public required Employee Employee { get; set; }
+    public required DateOnly Date { get; set; }
+}
